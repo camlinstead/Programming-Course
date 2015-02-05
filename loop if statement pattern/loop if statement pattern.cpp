e69fa3605f66e826e50f628081c0298e3b3d6ff7
camlinstead <camlinstead@msn.com>
2015-02-04 18:55:30 -0700

    Update loop if statement pattern.cpp

--- loop if statement pattern/loop if statement pattern.cpp
+++ loop if statement pattern/loop if statement pattern.cpp
@@ -1,35 +1,64 @@
 // loop if statement pattern.cpp : Defines the entry point for the console application.
 //
 
+
+
+
 #include "stdafx.h"
 #include <iostream>
-using namespace std;
+#include <string>
+
+using namespace std; 
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	char quit;
+	int cnt = 0; 
+	string xx = "";
+	int cnt2 = 9;
 
 
-	//first triangle
-	for (int r = 0; r < 10; r++)
+	while (cnt < 20)
 	{
-		for (int c = 0; c < r; c++)
-		{
-			cout << "*";
-		}
-		cout << endl;
+	if (cnt > 9)
+	{
+	cout << xx.substr((0), cnt2) << endl;
+	cnt2 --;
+	
+	}
+	else{
+	xx = xx + "*";
+
+	cout << xx << endl;
+	}
+	cnt++; 
+	//cnt = cnt+2; this is how i count by two's
 	}
 
-	//second triangle
-	for (int r = 10; r > 0; r--)
-	{
-		for (int c = 0; c < r; c++)
-		{
-			cout << "*";
-		}
-		cout << endl;
-	}
-	system("PAUSE");
+	cout << "Press any letter to quit ";
+	cin >> quit;
+
+	////first triangle
+	//for (int r = 0; r < 10; r++)
+	//{
+	//	for (int c = 0; c < r; c++)
+	//	{
+	//		cout << "*";
+	//	}
+	//	cout << endl;
+	//}
+
+	////second triangle
+	//for (int r = 10; r > 0; r--)
+	//{
+	//	for (int c = 0; c < r; c++)
+	//	{
+	//		cout << "*";
+	//	}
+	//	cout << endl;
+	//}
+	//system("PAUSE");
 
 	return 0;
 }
-
