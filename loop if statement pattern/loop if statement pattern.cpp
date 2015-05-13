9ff88edb1fce79868c608bab05b8644a9dfec751
camlinstead <camlinstead@msn.com>
2015-05-13 15:51:44 -0600

    Update loop if statement pattern.cpp

--- loop if statement pattern/loop if statement pattern.cpp
+++ loop if statement pattern/loop if statement pattern.cpp
@@ -1,64 +1,10 @@
-// loop if statement pattern.cpp : Defines the entry point for the console application.
-//
+	double ans;
+	double avg;
+
+	avg = (num1 + num2 + num3 + num4 + num5) / 5;
 
 
+	ans = sqrt((pow((num1-avg),2)+pow((num2-avg),2)+pow((num3-avg),2)+pow((num4-avg),2)+pow((num5-avg),2)) / 5 );
 
-
-#include "stdafx.h"
-#include <iostream>
-#include <string>
-
-using namespace std; 
-
-
-int _tmain(int argc, _TCHAR* argv[])
-{
-	char quit;
-	int cnt = 0; 
-	string xx = "";
-	int cnt2 = 9;
-
-
-	while (cnt < 20)
-	{
-	if (cnt > 9)
-	{
-	cout << xx.substr((0), cnt2) << endl;
-	cnt2 --;
+	return ans;
 	
-	}
-	else{
-	xx = xx + "*";
-
-	cout << xx << endl;
-	}
-	cnt++; 
-	//cnt = cnt+2; this is how i count by two's
-	}
-
-	cout << "Press any letter to quit ";
-	cin >> quit;
-
-	////first triangle
-	//for (int r = 0; r < 10; r++)
-	//{
-	//	for (int c = 0; c < r; c++)
-	//	{
-	//		cout << "*";
-	//	}
-	//	cout << endl;
-	//}
-
-	////second triangle
-	//for (int r = 10; r > 0; r--)
-	//{
-	//	for (int c = 0; c < r; c++)
-	//	{
-	//		cout << "*";
-	//	}
-	//	cout << endl;
-	//}
-	//system("PAUSE");
-
-	return 0;
-}
